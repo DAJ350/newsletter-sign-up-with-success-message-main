ca18113eb73f7d13b08bf49ac5c6f85e4b00e56b
DAJ350 <daniellajorin@gmail.com>
2024-10-29 06:50:36 +0000

    Form validation logic update

---
+++ script.js
@@ -0,0 +1,33 @@
+const emailInput = document.getElementById("email");
+const submitButton = document.getElementById("submit-btn");
+const errorStyles = [...document.getElementsByClassName("error")];
+const errorMessage = document.getElementById("error-message");
+const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const customEmail = document.getElementById("email-value");
+
+function formValidation(e) {
+  if (emailInput.value == "") {
+    e.preventDefault();
+    console.log("No input value");
+    errorMessage.innerHTML = "Please enter an email address";
+    errorStyles.forEach((element) => (element.style.display = "inline-block"));
+    emailInput.style.backgroundColor = "rgba(255, 97, 85, 15%)";
+    emailInput.style.borderColor = "var(--vermellion)";
+    emailInput.style.color = "var(--vermellion)";
+  } else if (!emailInput.value.match(emailPattern)) {
+    e.preventDefault();
+    console.log("Email format incorrect");
+    errorMessage.innerHTML = "Valid email required";
+    errorStyles.forEach((element) => (element.style.display = "inline-block"));
+    emailInput.style.backgroundColor = "rgba(255, 97, 85, 15%)";
+    emailInput.style.borderColor = "var(--vermellion)";
+    emailInput.style.color = "var(--vermellion)";
+  }
+}
+
+emailInput.addEventListener("input", (e) => {
+  e.target.style.backgroundColor = "var(--white)";
+  e.target.style.borderColor = "var(--grey-25)";
+});
+
+submitButton.addEventListener("click", formValidation);
